9dc85b9a32ba78b8296b519fcd2066e3ea5ed43c
Денис <DenisKhr@yandex.ru>
2023-06-12 17:46:37 +1000

    Добавил else в 4 и 5 задачу

--- src/Main.java
+++ src/Main.java
@@ -44,19 +44,19 @@ public class Main {
         if(age<2){
             System.out.println("Если возраст человека равен "+age+", то ему пора спать.");
         }
-        if(age>=2&&age<6){
+        else if(age>=2&&age<6){
             System.out.println("Если возраст человека равен "+age+", то ему нужно ходить в детский сад.");
         }
-        if(age>=7&age<18){
+        else if(age>=7&age<18){
             System.out.println("Если возраст человека равен "+age+", то ему нужно ходить в школу.");
         }
-        if(age>=18&age<24){
+        else if(age>=18&age<24){
             System.out.println("Если возраст человека равен "+age+", то его место в университете.");
         }
-        if(age>=24&age<60){
+        else if(age>=24&age<60){
             System.out.println("Если возраст человека равен "+age+", то ему пора ходить на работу.");
         }
-        if(age>=60){
+        else if(age>=60){
             System.out.println("Если возраст человека равен "+age+", то он может отдохнуть.");
         }
     }
@@ -67,10 +67,10 @@ public class Main {
         if (age < 5) {
             System.out.println("Если возраст ребенка равен " + age + ", то он не может кататься на аттракционе.");
         }
-        if (canOnlyRideIfAccompaniedByAnAdult) {
+        else if (canOnlyRideIfAccompaniedByAnAdult) {
             System.out.println("Если возраст ребенка равен " + age + ", то он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя.");
         }
-        if (age >= 14) {
+        else if (age >= 14) {
             System.out.println("Если возраст ребенка равен " + age + ", то он может кататься без сопровождения взрослого.");
         }
     }
